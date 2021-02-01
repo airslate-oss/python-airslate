51bde0333878cd28082822e02beebcfd14447219
Serghei Iakovlev <egrep@protonmail.ch>
2021-02-01 02:56:15 +0200

    Amend tests

--- tests/entities/test_documents.py
+++ tests/entities/test_documents.py
@@ -5,7 +5,7 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-from airslate.entities.documents import Document
+from airslate.entities.documents import Document, Field
 
 
 def test_document_assign_includes(documents_collection):
@@ -14,3 +14,13 @@ def test_document_assign_includes(documents_collection):
     assert len(documents) == 1
     assert isinstance(documents[0], Document)
     assert len(documents[0].included) == 2
+
+
+def test_document_has_many_fields(documents_collection):
+    document = Document.from_collection(documents_collection)[0]
+    fields = document.fields
+
+    assert isinstance(fields, list)
+    assert len(fields) == 2
+    assert isinstance(fields[0], Field)
+    assert isinstance(fields[1], Field)
