41aec238872ea2f51ff6c57dc4f35728b3ee6ef1
Serghei Iakovlev <egrep@protonmail.ch>
2021-02-13 13:59:41 +0200

    Provide Assign tags model

---
+++ airslate/models/tags.py
@@ -0,0 +1,34 @@
+# This file is part of the airslate.
+#
+# Copyright (c) 2021 airSlate, Inc.
+#
+# For the full copyright and license information, please view
+# the LICENSE file that was distributed with this source code.
+
+"""The base module for tags related models and functionality."""
+
+from asdicts.dict import path
+
+from .base import BaseModel
+
+
+class Assign(BaseModel):
+    """Represents Assign tags model."""
+
+    def set_names(self, names):
+        """Setter for tags names."""
+        self.data['names'] = names
+
+    def to_dict(self):
+        """Convert this model to a dictionary."""
+        tags = []
+
+        for name in path(self.data, 'names', []):
+            tags.append({
+                'type': 'flow_tags',
+                'attributes': {
+                    'name': name,
+                },
+            })
+
+        return {'data': tags}
