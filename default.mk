c2268a971f2752e5701336e8a900b0261ce32652
Serghei Iakovlev <egrep@protonmail.ch>
2023-03-25 12:47:34 +0100

    feat: revamped requirements files

--- default.mk
+++ default.mk
@@ -1,6 +1,6 @@
 # This file is part of the airslate.
 #
-# Copyright (c) 2021-2022 airSlate, Inc.
+# Copyright (c) 2021-2023 airSlate, Inc.
 #
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
@@ -11,23 +11,26 @@
 ROOT_DIR := $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
 PKG_NAME = airslate
 
-ifneq (,$(findstring xterm,${TERM}))
-	GREEN := $(shell tput -Txterm setaf 2)
-	RESET := $(shell tput -Txterm sgr0)
+PYLINT_FLAGS ?=
+FLAKE8_FLAGS ?= --show-source --statistics
+
+ifneq ($(TERM),)
+	GREEN := $(shell tput setaf 2)
+	RESET := $(shell tput sgr0)
 	CS = "${GREEN}~~~ "
 	CE = " ~~~${RESET}"
+
+	PYTEST_FLAGS ?= --color=yes
 else
 	CS = "~~~ "
 	CE = " ~~~"
+
+	PYTEST_FLAGS ?= --color=no
 endif
 
-COV          =
 HEADER_EXTRA =
-
-REQUIREMENTS = requirements-dev.txt
-
-PYTEST_FLAGS ?= --color=yes -v
-FLAKE8_FLAGS ?= --show-source --statistics
+COV          =
+REQUIREMENTS = requirements/requirements.txt requirements/requirements-dev.txt
 
 ifneq ($(VIRTUAL_ENV),)
 	VENV_ROOT = $(VIRTUAL_ENV)
@@ -37,15 +40,19 @@ endif
 
 # PYTHON will used to create venv
 ifeq ($(OS),Windows_NT)
-	PYTHON  ?= python
+	PYTHON  ?= python.exe
+	VIRTUALENV ?= virtualenv.exe
 	VENV_BIN = $(VENV_ROOT)/Scripts
 else
 	PYTHON  ?= python3
+	VIRTUALENV ?= $(PYTHON) -m venv
 	VENV_BIN = $(VENV_ROOT)/bin
 endif
 
-VENV_PIP    = $(VENV_BIN)/pip
 VENV_PYTHON = $(VENV_BIN)/python
+VENV_PIP    = $(VENV_PYTHON) -m pip
+
+export PATH := $(VENV_BIN):$(PATH)
 
 # Program availability
 ifndef PYTHON
@@ -54,7 +61,7 @@ else
 ifneq ($(OS),Windows_NT)
 HAVE_PYTHON := $(shell sh -c "command -v $(PYTHON)")
 ifndef HAVE_PYTHON
-$(error "Python is not available please install Python")
+$(error "Python is not available. Please install Python.")
 endif
 endif
 endif
