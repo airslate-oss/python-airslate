d68e9e1580d9ebde61162fe1f004c354e81a9ace
Serghei Iakovlev <egrep@protonmail.ch>
2023-03-26 03:27:28 +0200

    fix!: remove no longer needed models

--- airslate/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# This file is part of the airslate.
-#
-# Copyright (c) 2021-2023 airSlate, Inc.
-#
-# For the full copyright and license information, please view
-# the LICENSE file that was distributed with this source code.
-
-"""The top-level module for request models."""
