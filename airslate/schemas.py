be146f2775051bf1dc43d83adb287f32cb7bf1ba
Serghei Iakovlev <egrep@protonmail.ch>
2023-03-30 09:24:03 +0200

    feat: provide ability to get organization settings

--- airslate/schemas.py
+++ airslate/schemas.py
@@ -7,18 +7,22 @@
 
 """Schemas for handling (de)serialized model representation."""
 
-from marshmallow import fields, post_load, RAISE, Schema
+from marshmallow import fields, post_load, EXCLUDE, Schema
 
-from .models import Organization
+from .models import (
+    Organization,
+    OrganizationSettings,
+    OrganizationSettingsContent,
+)
 
 
 class OrganizationSchema(Schema):
-    """Schema for Organization model."""
+    """Schema for :class:`Organization` model."""
 
     class Meta:  # pylint: disable=too-few-public-methods
-        """Metaclass to setup OrganizationSchema."""
+        """Metaclass to setup :class:`OrganizationSchema`."""
 
-        unknown = RAISE
+        unknown = EXCLUDE
         datetimeformat = '%Y-%m-%dT%H:%M:%S.%f%z'
 
     id = fields.Str(required=True)
@@ -32,5 +36,42 @@ class OrganizationSchema(Schema):
 
     @post_load
     def make(self, data, **_kwargs):
-        """Deserialize the ``data`` to a Category instance."""
+        """Create a :class:`Organization` instance."""
         return Organization(**data)
+
+
+class OrganizationSettingContentSchema(Schema):
+    """Schema for :class:`OrganizationSettingsContent` model."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Create a :class:`OrganizationSettingContentSchema`."""
+
+        unknown = EXCLUDE
+
+    allow_recipient_registration = fields.Bool(required=True)
+    attach_completion_certificate = fields.Bool(required=True)
+    require_electronic_signature_consent = fields.Bool(required=True)
+    allow_reusable_flow = fields.Bool(required=True)
+    verified_domains = fields.List(fields.Str(), required=True)
+
+    @post_load
+    def make(self, data, **_kwargs) -> OrganizationSettingsContent:
+        """Create a :class:`OrganizationSettingsContent` instance."""
+        return OrganizationSettingsContent(**data)
+
+
+class OrganizationSettingSchema(Schema):
+    """Schema for :class:`OrganizationSettings` model."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Metaclass to setup :class:`OrganizationSettingSchema`."""
+
+    unknown = EXCLUDE
+
+    id = fields.Str(required=True)
+    settings = fields.Nested(OrganizationSettingContentSchema)
+
+    @post_load
+    def make(self, data, **_kwargs) -> OrganizationSettings:
+        """Create a :class:`OrganizationSettings` instance."""
+        return OrganizationSettings(**data)
